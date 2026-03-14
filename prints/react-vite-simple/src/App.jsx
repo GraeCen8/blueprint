8af30156c2f8322badf209daa312dbd7514d501a
grae ceney <gceney7@gmail.com>
2026-03-14 06:55:27 +0000

    Add react vite example app

---
+++ prints/react-vite-simple/src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>React Vite Simple</h1>
+      <p>Edit src/App.jsx and save to test HMR.</p>
+    </div>
+  );
+}
